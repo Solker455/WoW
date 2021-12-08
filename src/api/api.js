58a0b2350225ed1b5d90b81e630e9e6d315eb7ec
Dmitry <Spooner555@yandex.ru>
2021-12-08 17:17:51 +0300

    edit comments api, edit api function

--- src/api/api.js
+++ src/api/api.js
@@ -1,32 +1,32 @@
 import axios from "axios";
 
-export async function getPvpStats(token, pvpLadder) {
-    const url = `https://eu.api.blizzard.com/data/wow/pvp-season/31/pvp-leaderboard/${pvpLadder}?namespace=dynamic-eu&locale=ru_EU&access_token=${token}`;
+export function getPvpStats(token, pvpLadder) {
+    const url = `https://eu.api.blizzard.com/data/wow/pvp-season/31/pvp-leaderboard/${pvpLadder}?namespace=dynamic-eu&locale=ru_EU&access_token=${token}`; //получение пвп статистики из api Blizzard
     return axios.get(url)
 }
 
-export async function getPveStats(token, fraction, raid) {
-    const url = `https://eu.api.blizzard.com/data/wow/leaderboard/hall-of-fame/${raid}/${fraction}?namespace=dynamic-eu&locale=ru_EU&access_token=${token}`;
+export function getPveStats(token, fraction, raid) {
+    const url = `https://eu.api.blizzard.com/data/wow/leaderboard/hall-of-fame/${raid}/${fraction}?namespace=dynamic-eu&locale=ru_EU&access_token=${token}`; //получение пве статистики из api Blizzard
     return axios.get(url)
 }
 
-export async function apiCode() {
-    document.location.href = 'http://localhost:8000/login';
+export function apiCode() {
+    document.location.href = 'http://localhost:8000/login'; //получение токена авторизации
 }
 
-export async function apiUserInfo(token) {
-    const url = `https://eu.battle.net/oauth/userinfo?region=eu&access_token=${token}`;
+export function apiUserInfo(token) {
+    const url = `https://eu.battle.net/oauth/userinfo?region=eu&access_token=${token}`; //получение battletag из api Blizzard
     return axios.get(url)
 }
 
-export async function getHeroUser(token) {
-    const url = `https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&locale=ru_EU&access_token=${token}`;
-    return await axios.get(url)
+export function getHeroUser(token) {
+    const url = `https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&locale=ru_EU&access_token=${token}`; //получение списка персонажей пользователя из api Blizzard
+    return axios.get(url)
 }
 
-export async function getCharacterStats(token, name, realm) {
-    const url = `https://eu.api.blizzard.com/profile/wow/character/${realm}/${name}/statistics?namespace=profile-eu&locale=ru_EU&access_token=${token}`;
-    return await axios.get(url).catch(error => {
+export function getCharacterStats(token, name, realm) {
+    const url = `https://eu.api.blizzard.com/profile/wow/character/${realm}/${name}/statistics?namespace=profile-eu&locale=ru_EU&access_token=${token}`; //получение статистики персонажа из api Blizzard
+    return axios.get(url).catch(error => {
         return error
     })
 }
